f59afe0ba2f48c7f6ddcb3978c400ff48533fd56
jeremyg <jeremygirard89100@gmail.com>
2025-12-08 15:52:28 +0100

    add sqlConnect and session.php

--- index.php
+++ index.php
@@ -1,46 +1,3 @@
-<?php
-/*
-namespace App\Models;
-
-use \PDO;
-
-class SqlConnect {
-  public object $db;
-  private string $host;
-  private string $port;
-  private string $dbname;
-  private string $password;
-  private string $user;
-
-  public function __construct() {
-    $this->host = '127.0.0.1';
-    $this->port = '3306';
-    $this->dbname = 'demo';
-    $this->user = 'root';
-    $this->password = 'root';
-
-    $this->db = new PDO(
-      'mysql:host='.$this->host.';port='.$this->port.';dbname='.$this->dbname,
-      $this->user,
-      $this->password
-    );
-
-    $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $this->db->setAttribute(PDO::ATTR_PERSISTENT, false);
-  }
-
-  public function transformDataInDot($data) {
-    $dataFormated = [];
-
-    foreach ($data as $key => $value) {
-      $dataFormated[':' . $key] = $value;
-    }
-
-    return $dataFormated;
-  }
-}
-*/
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -51,8 +8,6 @@ class SqlConnect {
 </head>
 
 <body>
-
-
     <button name="j1" method="post">Devenir joueur 1</button>
     <button name="j2" method="post">Devenir joueur 2</button>
 </body>
