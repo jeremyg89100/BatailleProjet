f1c5d49a47fe4ca663267f101691f0a5fbc2c8a2
MaximePetit03 <doudoumax2008@gmail.com>
2025-12-08 15:36:26 +0100

    Plateau

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 namespace App\Models;
 
 use \PDO;
@@ -39,6 +39,7 @@ class SqlConnect {
     return $dataFormated;
   }
 }
+*/
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -50,157 +51,10 @@ class SqlConnect {
 </head>
 
 <body>
-    <main>
-        <table>
-            <caption>
-                J1
-            </caption>
-            <tr>
-                <th></th>
-                <th scope="col">1</th>
-                <th scope="col">2</th>
-                <th scope="col">3</th>
-                <th scope="col">4</th>
-                <th scope="col">5</th>
-                <th scope="col">6</th>
-                <th scope="col">7</th>
-                <th scope="col">8</th>
-                <th scope="col">9</th>
-                <th scope="col">10</th>
-            </tr>
-            <tr>
-                <th scope="row">A</th>
-                <td><button name="A1" method="post">A1</button></td>
-                <td><button name="A2" method="post">A2</button></td>
-                <td><button name="A3" method="post">A3</button></td>
-                <td><button name="A4" method="post">A4</button></td>
-                <td><button name="A5" method="post">A5</button></td>
-                <td><button name="A6" method="post">A6</button></td>
-                <td><button name="A7" method="post">A7</button></td>
-                <td><button name="A8" method="post">A8</button></td>
-                <td><button name="A9" method="post">A9</button></td>
-                <td><button name="A10" method="post">A10</button></td>
-            </tr>
-            <tr>
-                <th scope="row">B</th>
-                <td><button name="B1" method="post">B1</button></td>
-                <td><button name="B2" method="post">B2</button></td>
-                <td><button name="B3" method="post">B3</button></td>
-                <td><button name="B4" method="post">B4</button></td>
-                <td><button name="B5" method="post">B5</button></td>
-                <td><button name="B6" method="post">B6</button></td>
-                <td><button name="B7" method="post">B7</button></td>
-                <td><button name="B8" method="post">B8</button></td>
-                <td><button name="B9" method="post">B9</button></td>
-                <td><button name="B10" method="post">B10</button></td>
-            </tr>
-            <tr>
-                <th scope="row">C</th>
-                <td><button name="C1" method="post">C1</button></td>
-                <td><button name="C2" method="post">C2</button></td>
-                <td><button name="C3" method="post">C3</button></td>
-                <td><button name="C4" method="post">C4</button></td>
-                <td><button name="C5" method="post">C5</button></td>
-                <td><button name="C6" method="post">C6</button></td>
-                <td><button name="C7" method="post">C7</button></td>
-                <td><button name="C8" method="post">C8</button></td>
-                <td><button name="C9" method="post">C9</button></td>
-                <td><button name="C10" method="post">C10</button></td>
-            </tr>
-            <tr>
-                <th scope="row">D</th>
-                <td><button name="D1" method="post">D1</button></td>
-                <td><button name="D2" method="post">D2</button></td>
-                <td><button name="D3" method="post">D3</button></td>
-                <td><button name="D4" method="post">D4</button></td>
-                <td><button name="D5" method="post">D5</button></td>
-                <td><button name="D6" method="post">D6</button></td>
-                <td><button name="D7" method="post">D7</button></td>
-                <td><button name="D8" method="post">D8</button></td>
-                <td><button name="D9" method="post">D9</button></td>
-                <td><button name="D10" method="post">D10</button></td>
-            </tr>
-            <tr>
-                <th scope="row">E</th>
-                <td><button name="E1" method="post">E1</button></td>
-                <td><button name="E2" method="post">E2</button></td>
-                <td><button name="E3" method="post">E3</button></td>
-                <td><button name="E4" method="post">E4</button></td>
-                <td><button name="E5" method="post">E5</button></td>
-                <td><button name="E6" method="post">E6</button></td>
-                <td><button name="E7" method="post">E7</button></td>
-                <td><button name="E8" method="post">E8</button></td>
-                <td><button name="E9" method="post">E9</button></td>
-                <td><button name="E10" method="post">E10</button></td>
-            </tr>
-            <tr>
-                <th scope="row">F</th>
-                <td><button name="F1" method="post">F1</button></td>
-                <td><button name="F2" method="post">F2</button></td>
-                <td><button name="F3" method="post">F3</button></td>
-                <td><button name="F4" method="post">F4</button></td>
-                <td><button name="F5" method="post">F5</button></td>
-                <td><button name="F6" method="post">F6</button></td>
-                <td><button name="F7" method="post">F7</button></td>
-                <td><button name="F8" method="post">F8</button></td>
-                <td><button name="F9" method="post">F9</button></td>
-                <td><button name="F10" method="post">F10</button></td>
-            </tr>
-            <tr>
-                <th scope="row">G</th>
-                <td><button name="G1" method="post">G1</td>
-                <td><button name="G2" method="post">G2</td>
-                <td><button name="G3" method="post">G3</td>
-                <td><button name="G4" method="post">G4</td>
-                <td><button name="G5" method="post">G5</td>
-                <td><button name="G6" method="post">G6</td>
-                <td><button name="G7" method="post">G7</td>
-                <td><button name="G8" method="post">G8</td>
-                <td><button name="G9" method="post">G9</td>
-                <td><button name="G10" method="post">G10</td>
-            </tr>
-            <tr>
-                <th scope="row">H</th>
-                <td><button name="H1" method="post">H1</td>
-                <td><button name="H2" method="post">H2</td>
-                <td><button name="H3" method="post">H3</td>
-                <td><button name="H4" method="post">H4</td>
-                <td><button name="H5" method="post">H5</td>
-                <td><button name="H6" method="post">H6</td>
-                <td><button name="H7" method="post">H7</td>
-                <td><button name="H8" method="post">H8</td>
-                <td><button name="H9" method="post">H9</td>
-                <td><button name="H10" method="post">H10</td>
-            </tr>
 
-            <tr>
-                <th scope="row">I</th>
-                <td><button name="I1" method="post">I1</td>
-                <td><button name="I2" method="post">I2</td>
-                <td><button name="I3" method="post">I3</td>
-                <td><button name="I4" method="post">I4</td>
-                <td><button name="I5" method="post">I5</td>
-                <td><button name="I6" method="post">I6</td>
-                <td><button name="I7" method="post">I7</td>
-                <td><button name="I8" method="post">I8</td>
-                <td><button name="I9" method="post">I9</td>
-                <td><button name="I10" method="post">I10</td>
-            </tr>
-            <tr>
-                <th scope="row">J</th>
-                <td><button name="J1" method="post">J1</td>
-                <td><button name="J2" method="post">J2</td>
-                <td><button name="J3" method="post">J3</td>
-                <td><button name="J4" method="post">J4</td>
-                <td><button name="J5" method="post">J5</td>
-                <td><button name="J6" method="post">J6</td>
-                <td><button name="J7" method="post">J7</td>
-                <td><button name="J8" method="post">J8</td>
-                <td><button name="J9" method="post">J9</td>
-                <td><button name="J10" method="post">J10</td>
-            </tr>
-        </table>
-    </main>
+
+    <button name="j1" method="post">Devenir joueur 1</button>
+    <button name="j2" method="post">Devenir joueur 2</button>
 </body>
 
 </html>
