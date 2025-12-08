5152cbf4f4c5e93f8e0e2068c181b6de6305cb0e
jeremyg <jeremygirard89100@gmail.com>
2025-12-08 14:34:50 +0100

    add script.php instanciation clase

---
+++ script.php
@@ -0,0 +1,6 @@
+<?php
+
+    $torpilleur = new Bateau(1, "torpilleur", 2);
+    $sousMarin = new Bateau(2, "sous-marin", 3);
+    $croiseur = new Bateau(3, "croiseur", 4);
+    $porteAvion = new Bateau(4, "porte-avion", 5);
